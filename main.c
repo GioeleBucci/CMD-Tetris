cbb064ac2beebb838d9b7fb2f713471becb86394
GioeleBucci <gioelebucci@gmail.com>
2023-08-01 10:06:56 +0200

    Add tetromino collision and falling
    - Added a simple game loop that makes current tetromino continuously fall and places it when it hits something

--- main.c
+++ main.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <time.h>
+#include <conio.h>
+#include <time.h>
 
 #include "points2D.h"
 
 #define WIDTH 12
 #define HEIGHT 22
+
+#define FPS 6
 
 
 typedef struct Game {
@@ -20,30 +24,14 @@ enum colors {
     AIR, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
 };
 
-int currentPieceRow = 1, currentPieceCol = WIDTH / 2;
+int currentPieceRow, currentPieceCol;
+int currentPieceType;
+
 
 char pieces[7][4][4] = {
-        { // I
-                "    ",
-                "####",
-                "    ",
-                "    "
-        },
-        { // J
-                "#   ",
-                "### ",
-                "    ",
-                "    "
-        },
-        { // L
-                "  # ",
-                "### ",
-                "    ",
-                "    "
-        },
-        { // O
+        { // Z
                 "##  ",
-                "##  ",
+                " ## ",
                 "    ",
                 "    "
         },
@@ -53,15 +41,33 @@ char pieces[7][4][4] = {
                 "    ",
                 "    "
         },
+        { // O
+                "##  ",
+                "##  ",
+                "    ",
+                "    "
+        },
+        { // J
+                "#   ",
+                "### ",
+                "    ",
+                "    "
+        },
         { // T
                 " #  ",
                 "### ",
                 "    ",
                 "    "
         },
-        { // Z
-                "##  ",
-                " ## ",
+        { // I
+                "    ",
+                "####",
+                "    ",
+                "    "
+        },
+        { // L
+                "  # ",
+                "### ",
                 "    ",
                 "    "
         }
@@ -70,36 +76,52 @@ char pieces[7][4][4] = {
 /* --------------------------------------------------------------------------------- */
 
 /// Function to print the colored character
-void printColoredChar(unsigned char ch, int color) {
-    printf("\x1b[3%dm%c\x1b[0m", color, ch);
+void printColoredChar(unsigned char ch, int pieceType) {
+    printf("\x1b[3%dm%c\x1b[0m", (pieceType == 6 ? YELLOW : pieceType + 1), ch);
+}
+
+void generateNewTetromino(){
+    currentPieceType = rand()%7;
+    currentPieceRow = 1;
+    currentPieceCol = WIDTH / 2 - 1;
 }
 
 void initGrid(Game *game) {
+
     for (int i = 0; i < HEIGHT; ++i)
         for (int j = 0; j < WIDTH; ++j)
             game->grid[i][j] = WHITE; // walls
     for (int i = 1; i < HEIGHT - 1; ++i)
         for (int j = 1; j < WIDTH - 1; ++j)
             game->grid[i][j] = AIR;
+
+    //start random number generator
+    time_t t;
+    time(&t);
+    srand((unsigned int)t);
+
+    //generate first piece
+    generateNewTetromino();
 }
 
 /// returns non-zero if a tetromino piece is rendered
 int printTetromino(int i, int j, int type) {
     int row = i - currentPieceRow;
     int col = j - currentPieceCol;
-    if (row < 4 && row >= 0 && col < 4 && col>=0 && pieces[type][row][col] != ' ') {
-        printColoredChar(pieces[type][row][col], BLUE);
+    if (row < 4 && row >= 0 && col < 4 && col >= 0 && pieces[type][row][col] != ' ') {
+        // since the terminal has no orange both O and L pieces use yellow
+        printColoredChar(219, type);
         return 1;
     }
     return 0;
 }
 
 void refresh(Game *game) {
-
+    system("cls");
     // PRINT GRID
     for (int i = 0; i < HEIGHT; ++i) {
         for (int j = 0; j < WIDTH; ++j) {
-            if (!printTetromino(i, j, 4)) {
+            if (!printTetromino(i, j, currentPieceType)) {
                 if (game->grid[i][j] == AIR)
                     printf(" ");
                 else // in game->grid[i][j] is stored the color of the single block
@@ -123,22 +145,70 @@ void dropPiece() {
     //placePiece();
 }
 
-void fall(Game *game) {
-    for (int i = 0; i < HEIGHT; ++i) {
-        for (int j = 0; j < WIDTH; ++j) {
-            if (game->grid[i][j] == '#') {
-                game->grid[i + 1][j] = 'f';
+bool isCollision(const Game *game, int rowOffset, int colOffset, int pieceType) {
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            // found block
+            if (pieces[pieceType][i][j] != ' ') {
+                int row = currentPieceRow + i + rowOffset;
+                int col = currentPieceCol + j + colOffset;
+                if (game->grid[row][col] != AIR)
+                    return true;
             }
+        }
+    }
+    return false;
+}
+
+Point2D getInputs() {
+    int input;
+    if (kbhit())
+        input = tolower(getch()); //kbhit() is a non-blocking input function
+    Point2D dir;
+    if (input == 'w') dir = newPoint2D(-1, 0);
+    if (input == 'a') dir = newPoint2D(0, -1);
+    if (input == 's') dir = newPoint2D(1, 0);
+    if (input == 'd') dir = newPoint2D(0, 1);
+
+    return dir;
+}
+
+void placeTetromino(Game *game, int type){
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            if(pieces[type][i][j] != ' ')
+                game->grid[currentPieceRow+i][currentPieceCol+j] = type;
         }
     }
 }
 
 
+
+
 int main() {
     Game game;
     initGrid(&game);
-    //generate a piece
-    //placePieceAt(&game, 1, 1, 1);
     refresh(&game);
-    //fall(&game);
+
+    clock_t t;
+    while (1/*currentPieceRow != HEIGHT - 3 */) {
+        t = clock();
+        fflush(stdin); //?
+        Point2D input = getInputs();
+        //check left or right movement
+        if(isSamePoint2D(input,POINT2D_LEFT) && !isCollision(&game,0,-1,currentPieceType))
+            currentPieceCol--;
+        else if(isSamePoint2D(input,POINT2D_RIGHT) && !isCollision(&game,0,1,currentPieceType))
+            currentPieceCol++;
+        if (!isCollision(&game, 1, 0, currentPieceType)) {
+            currentPieceRow++;
+        }else{
+            placeTetromino(&game,currentPieceType);
+            generateNewTetromino();
+        }
+        t = clock() - t;
+        double elapsedTime = ((double) t) / CLOCKS_PER_SEC * 1000;
+        Sleep((1000 - (int) elapsedTime) / FPS);
+        refresh(&game);
+    }
 }
--- points2D.h
+++ points2D.h
@@ -7,6 +7,11 @@ typedef struct Point2D {
     int yRow, xCol;
 } Point2D;
 
+#define POINT2D_UP newPoint2D(-1,0)
+#define POINT2D_DOWN newPoint2D(1,0)
+#define POINT2D_LEFT newPoint2D(0,-1)
+#define POINT2D_RIGHT newPoint2D(0,1)
+
 Point2D sumPoint2D(Point2D a, Point2D b);
 
 Point2D newPoint2D(int y, int x);
